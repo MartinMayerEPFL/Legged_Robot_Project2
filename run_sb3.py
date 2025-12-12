082496dade88af61f14e4f44b47cc2eb63b9642e
Your Name <you@example.com>
2025-12-12 16:57:26 +0100

    Test

--- run_sb3.py
+++ run_sb3.py
@@ -49,10 +49,10 @@ from utils.file_utils import get_latest_model
 # gym environment
 from env.quadruped_gym_env import QuadrupedGymEnv
 
-LEARNING_ALG = "PPO" # or "SAC"
+LEARNING_ALG = "SAC" # or "SAC"
 LOAD_NN = False # if you want to initialize training with a previous model 
 NUM_ENVS = 16    # how many pybullet environments to create for data collection
-USE_GPU = False # make sure to install all necessary drivers 
+USE_GPU = True # make sure to install all necessary drivers 
 
 # after implementing, you will want to test how well the agent learns with your MDP: 
 env_configs = {"motor_control_mode":"PD",
@@ -116,10 +116,10 @@ ppo_config = {  "gamma":0.99,
 
 # What are these hyperparameters? Check here: https://stable-baselines3.readthedocs.io/en/master/modules/sac.html
 sac_config={"learning_rate":1e-4,
-            "buffer_size":300000,
-            "batch_size":256,
+            "buffer_size":1000000,
+            "batch_size":512,
             "ent_coef":'auto', 
-            "gamma":0.99, 
+            "gamma":0.995, 
             "tau":0.005,
             "train_freq":1, 
             "gradient_steps":1,
